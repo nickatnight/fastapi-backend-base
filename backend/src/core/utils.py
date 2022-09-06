37507312d2198c293a4ce7a0556989e399a48301
nickatnight <kay.muneee@gmail.com>
2022-09-06 00:09:48 -0700

    [#8] feat: add poetry for dev

--- backend/src/core/utils.py
+++ backend/src/core/utils.py
@@ -1,3 +1,5 @@
+import platform
+
 from r_dogecoin_bot.main import DogecoinMemeBot
 from r_dogecoin_bot.types import RedditClientConfig
 
@@ -5,11 +7,14 @@ from src.core.config import settings
 from src.db.clients.pg_client import PostgresSubmissionDbClient
 
 
+platform_name = platform.uname()
+
+
 async def botnet_run():
     reddit_config: RedditClientConfig = dict(
         client_id=settings.CLIENT_ID,
         client_secret=settings.CLIENT_SECRET,
-        user_agent="to_the_moon",
+        user_agent=f"{platform_name.release} {platform_name.machine} v3/fastapi-backend-base",
     )
     bot = DogecoinMemeBot(
         reddit_client_config=reddit_config,
